1c47c781518913c28d71945be3fa8ba6d1cee0e9
Prateek <128773897+pmalviya1@users.noreply.github.com>
2024-02-18 21:01:16 +0530

    Update word_guessing_game.py

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -16,19 +16,27 @@ print('Welcome to the word guessing game!')
 print(f'Your word is of {len(secret_word)} letters')
 
 # game loop
-while wrong_guesses <= max_guesses:
+while wrong_guesses < max_guesses:
     
     # diplay current progress
     display_word = ''
+    
+    # Creating the current progress
     for c in secret_word:
-        if char in guesses:
-            display_word = display_word + char
+        if c in guesses:
+            display_word = display_word + c
         else:
-            display_word = display_word + '_'
+            display_word = display_word + '_ '
     print(f"Guess the word: {display_word}")
+    
     
     # get the user guess
     guess = input('Enter your guess (single letter): ').lower()
+    
+    # if input is valid or not
+    if len(guess) != 1 or not guess.isalpha():
+        print("Invalid guess. Please enter a single alphabet letter.")
+        continue
     
     # check if guess is correct
     if guess in secret_word:
@@ -36,14 +44,13 @@ while wrong_guesses <= max_guesses:
         print('Correct!')
     else:
         wrong_guesses += 1
-        print(f'You have {max_guesses-wrong_guesses} guesses left')
+        print(f'Wrong! You have {max_guesses-wrong_guesses} guesses left')
     
     # check if word is guessed or not
-    
     if display_word == secret_word:
-        print(f'Congratulations! You have guesses the word: {secret_word}')
+        print(f'Congratulations! You have guessed the word: {secret_word}')
         break
     
-# End of game message
-if wrong_guesses == max_guesses:
-    print(f'Sorry, you ran out of guesses. The word was: {secret_word}')
+    # End of game message
+    if wrong_guesses == max_guesses:
+        print(f'Sorry, you ran out of guesses. The word was: {secret_word}')
